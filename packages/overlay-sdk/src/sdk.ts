487a385314c297ac3e8732fe900c8ddf36f35f9d
Aliona-D <alona.danyliuk@inventorsoft.co>
2024-09-03 16:38:53 +0300

    Remove test module

--- packages/overlay-sdk/src/sdk.ts
+++ packages/overlay-sdk/src/sdk.ts
@@ -2,11 +2,9 @@ import { createPublicClient, http } from "viem";
 import { OverlaySDKCore, OverlaySDKCoreProps } from "./core/index.js";
 import { OverlaySDKMarket, OverlaySDKState } from "./markets/index.js";
 import { OverlaySDKMarkets } from './marketsList/index.js';
-import { TestModuleSDK } from "./test_module/index.js";
 import { arbitrumSepolia } from "viem/chains";
 export class OverlaySDK {
   readonly core: OverlaySDKCore;
-  readonly test_module: TestModuleSDK;
   readonly market: OverlaySDKMarket;
   readonly state: OverlaySDKState;
   readonly midPrice: OverlaySDKState;
@@ -16,8 +14,7 @@ export class OverlaySDK {
     // Core functionality
     this.core = new OverlaySDKCore(props);
     const core = this.core;
-    // Test Module functionality
-    this.test_module = new TestModuleSDK({ ...props, core });
+    
     this.market = new OverlaySDKMarket({ ...props, core });
     this.state = new OverlaySDKState({ ...props, core });
     this.midPrice = new OverlaySDKState({ ...props, core });
