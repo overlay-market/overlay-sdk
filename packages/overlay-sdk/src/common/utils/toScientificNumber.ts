110d0f2b6ff41943f42e6ee269b7383faff29966
Aliona-D <alona.danyliuk@inventorsoft.co>
2024-09-05 17:26:02 +0300

    Restore bignumber.js library in toScientificNumber file

--- packages/overlay-sdk/src/common/utils/toScientificNumber.ts
+++ packages/overlay-sdk/src/common/utils/toScientificNumber.ts
@@ -1,101 +1,86 @@
-import { formatUnits } from 'viem';
+import BigNumber from 'bignumber.js'
 
-const BIG_ZERO = 0n;
-const BIG_ONE = 1n;
+const BIG_ZERO = new BigNumber(0)
+const BIG_ONE = new BigNumber(1)
 
-const TRILLION = BIG_ONE * 10n ** 12n;
-const BILLION = BIG_ONE * 10n ** 9n;
-const MILLION = BIG_ONE * 10n ** 6n;
+const TRILLION = BIG_ONE.shiftedBy(12)
+const BILLION = BIG_ONE.shiftedBy(9)
+const MILLION = BIG_ONE.shiftedBy(6)
 
-const parseBigNumberOrZero = (
-  input: string | bigint | number | null
-) => {
-  if (!input) return BIG_ZERO;
+const parseBigNumberOrZero = (input: string | BigNumber | bigint | number | null) => {
+  if (!input) return BIG_ZERO
 
   // Remove any commas
-  let inputStr = String(input).replace(/,/g, "");
+  let inputStr = String(input).replace(/,/g, '')
 
-  let result: bigint;
+  const result = BigNumber.isBigNumber(inputStr) ? new BigNumber(inputStr) : new BigNumber(inputStr)
 
-  try {
-    result = BigInt(inputStr);
-  } catch {
-    return BIG_ZERO;
-  }
+  if (!result.isFinite() || result.isNaN()) return BIG_ZERO
 
-  return result;
-};
+  return result
+}
 
-const toPrecisionTrim = (value: bigint, significantFigures: number) => {
-  const valueStr = formatUnits(value, 0);
-  const isNegative = valueStr.startsWith("-");
-  const absValueStr = isNegative ? valueStr.slice(1) : valueStr;
- 
-  let formattedValue = Number(absValueStr)
-    .toPrecision(significantFigures)
-    .replace(/(\.[0-9]*?)0+$/, "$1");
+const toPrecisionTrim = (value: BigNumber, significantFigures: number) => {
+  const isNegative = value.isNegative()
+  const absValue = value.abs()
+
+  let formattedValue = absValue.toPrecision(significantFigures, 1).replace(/(\.[0-9]*?)0+$/, '$1')
 
   // Add back commas when necessary
-  const parts = formattedValue.split(".");
-  parts[0] = Number(parts[0]).toLocaleString();
-  formattedValue = parts.join(".");
+  const parts = formattedValue.split('.')
+  parts[0] = new BigNumber(parts[0]).toFormat()
+  formattedValue = parts.join('.')
 
-  formattedValue = formattedValue.endsWith(".")
-    ? formattedValue.slice(0, -1)
-    : formattedValue;
-  return isNegative ? `-${formattedValue}` : formattedValue;
-};
+  formattedValue = formattedValue.endsWith('.') ? formattedValue.slice(0, -1) : formattedValue
+  return isNegative ? `-${formattedValue}` : formattedValue
+}
 
-export const toScientificNumber = (
-  input: string | bigint | number | null | undefined
-): string => {
-  const value = parseBigNumberOrZero(input || 0);
+export const toScientificNumber = (input: string | BigNumber | bigint | number | null | undefined) => {
+  const value = parseBigNumberOrZero(input || 0)
 
   // Check if the value is zero
-  if (value === BIG_ZERO) {
-    return '0';
+  if (value.isZero()) {
+    return value.toFixed()
   }
 
   // Check if value less than 0.0001 for exponential
-  const absValue = BigInt(Math.abs(Number(formatUnits(value, 0))));
-  
-  if (absValue < 10n ** 4n) {
-    const sign = value < 0 ? "-" : "";
-    let exponent = 0n;
-    let baseValue = absValue;
+  if (value.abs().lt(0.0001)) {
+    const sign = value.isNegative() ? '-' : ''
+    let exponent = 0
+    let absValue = value.abs()
 
     // Multiply till base is > 100
-    while (baseValue < 100n) {
-      baseValue *= 10n;
-      exponent--;
+    while (absValue.lt(100)) {
+      absValue = absValue.times(10)
+      exponent--
     }
 
-    return `${sign}${toPrecisionTrim(baseValue, 3)}e-${-exponent}`;
+    return `${sign}${toPrecisionTrim(absValue, 3)}e-${-exponent}`
   }
 
   // Check if the value is less than 1 million
-  if (absValue < MILLION) {
-    return toPrecisionTrim(value, 6);
+  if (value.abs().lt(MILLION)) {
+    return toPrecisionTrim(value, 6)
   }
 
   // Check if the value is less than 1 billion
-  if (absValue < BILLION) {
-    return `${toPrecisionTrim(value / MILLION, 6)} M`;
+  if (value.abs().lt(BILLION)) {
+    return `${toPrecisionTrim(value.div(MILLION), 6)} M`
   }
 
   // Check if the value is less than 1 trillion
-  if (absValue < TRILLION) {
-    return `${toPrecisionTrim(value / BILLION, 6)} B`;
+  if (value.abs().lt(TRILLION)) {
+    return `${toPrecisionTrim(value.div(BILLION), 6)} B`
   }
 
   // Value is greater than or equal to 1 trillion
-  return `${toPrecisionTrim(value / TRILLION, 6)} T`;
-};
+  return `${toPrecisionTrim(value.div(TRILLION), 6)} T`
+}
 
 export const toPercentUnit = (input: string | number | null | undefined) => {
-  const ONE_HUNDRED = 100;
+  const ONE_HUNDRED = 100
 
-  if (Number.isNaN(Number(input))) return 0;
+  if (Number.isNaN(Number(input))) return 0
 
-  return (Number(input) * ONE_HUNDRED).toFixed(2);
-};
+  return (Number(input) * ONE_HUNDRED).toFixed(2)
+}
