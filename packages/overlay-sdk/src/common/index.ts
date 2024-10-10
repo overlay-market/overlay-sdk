838e751eb253072d201f4f47888b845bc76f991f
djaciel <dj_roses@hotmail.com>
2024-10-10 09:30:36 -0600

    add some utils to sdk root

--- packages/overlay-sdk/src/common/index.ts
+++ packages/overlay-sdk/src/common/index.ts
@@ -1,2 +1,5 @@
 export * from './constants.js';
 export * from './utils/sdk-error.js';
+export * from './utils/toScientificNumber.js';
+export * from './utils/formatWei.js';
+export * from './utils/limitDigitsInDecimals.js';
