ec9ebe227503ee7f2677e63bb45574316ff78826
djaciel <dj_roses@hotmail.com>
2024-10-21 21:24:13 -0600

    refactor SDKError class and add error description handling

--- packages/overlay-sdk/src/common/utils/sdk-error.ts
+++ packages/overlay-sdk/src/common/utils/sdk-error.ts
@@ -14,6 +14,10 @@ export type SDKErrorProps = {
   message?: string;
 };
 
+interface ErrorCause {
+  reason?: string;
+}
+
 export class SDKError extends Error {
   public static from(
     error: unknown,
@@ -35,12 +39,18 @@ export class SDKError extends Error {
 
   public code: ERROR_CODE;
   public errorMessage: string | undefined;
+  public errorDescription: string | undefined;
 
   constructor({ code, error = {}, message }: SDKErrorProps) {
     super(message);
     if (error instanceof Error) {
       this.cause = error.cause;
       this.stack = error.stack;
+
+      const cause = error.cause as ErrorCause;
+      if (cause && cause.reason) {
+        this.errorDescription = getErrorDescription(cause.reason);
+      }
     }
     this.code = code ?? ERROR_CODE.UNKNOWN_ERROR;
     this.errorMessage = message;
@@ -76,4 +86,55 @@ export async function withSDKError<TResult>(
   } catch (error) {
     throw SDKError.from(error, code);
   }
+}
+
+function getErrorDescription(reason: string): string {
+  let errorMessage = ''
+  switch (reason) {
+    case 'OVLV1: shutdown':
+      errorMessage = 'Market has been shutdown.'
+      break
+    case 'OVLV1:lev<min':
+      errorMessage = 'Leverage must be at least 1.'
+      break
+    case 'OVLV1:lev>max':
+      errorMessage = 'Leverage is greater than max.'
+      break
+    case 'OVLV1:fraction>max':
+      errorMessage = 'Fraction cannot equal greater than 1.'
+      break
+    case 'OVLV1:fraction<min':
+      errorMessage = 'Fraction cannot equal less than 0.'
+      break
+    case 'OVLV1:!position':
+      errorMessage = 'Position does not currently exist.'
+      break
+    case 'OVLV1:collateral<min':
+      errorMessage = 'Input collateral is less than minimum required collateral.'
+      break
+    case 'OVLV1:oi>cap':
+      errorMessage = 'Attempting to build position that will exceed market oi cap!'
+      break
+    case 'OVLV1:slippage>max':
+      errorMessage = 'User selected price limit is outside the range of the current market price.'
+      break
+    case 'OVLV1:!data':
+      errorMessage = 'Market price volatility has exceeded risk tolerance in the last hour.'
+      break
+    case 'OVLV1:oi==0':
+      errorMessage = 'Underlying price feed price points are too high. Unable to reliably estimate oi.'
+      break
+    case 'OVLV1:liquidatable':
+      errorMessage = 'Cannot build/unwind due to being liquidatable at execution time.'
+      break
+    case 'OVLV1:!liquidatable':
+      errorMessage = 'Position is currently not liquidatable.'
+      break
+    case 'ERC20: transfer amount exceeds balance':
+      errorMessage = 'Not enough balance.'
+      break
+    default:
+      errorMessage = 'Undefined error. Please look into console'
+  }
+  return errorMessage
 }
