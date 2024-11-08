dfb5990dc5c296388780a6716162f4fdb9a82103
djaciel <dj_roses@hotmail.com>
2024-11-07 18:42:49 -0600

    feat(index): export new types for IntervalType, OverviewData, TradeState, and UnwindState, and update references

--- packages/overlay-sdk/src/core/index.ts
+++ packages/overlay-sdk/src/core/index.ts
@@ -4,7 +4,6 @@ export { toPercentUnit, toScientificNumber } from '../common/utils/toScientificN
 export { limitDigitsInDecimals } from '../common/utils/limitDigitsInDecimals.js';
 export { SDKError, withSDKError } from '../common/utils/sdk-error.js';
 
-
 export { default as OverlaySDKCore } from './core.js';
 export type {
   OverlaySDKCoreProps,
@@ -12,3 +11,10 @@ export type {
   AccountValue,
   LOG_MODE,
 } from './types.js';
+
+export type { IntervalType, OverviewData, OverviewDataByPeriod } from '../account/index.js';
+export type { TradeStateData, UnwindStateSuccess, UnwindStateError, UnwindStateData } from '../trade/index.js';
+export { TradeState, UnwindState } from '../trade/index.js';
+export type { LiquidatedPositionData } from '../positionsTables/liquidatePositionsTable/liquidatePositionsTable.js';
+export type { OpenPositionData } from '../positionsTables/openPositionsTable/openPositionsTable.js';
+export type { UnwindPositionData } from '../positionsTables/unwindPositinosTable/unwindPositionsTable.js';
