7b7f1595cffdaf65462204e28ff4e5b496eb86b2
djaciel <dj_roses@hotmail.com>
2025-02-25 17:52:50 -0600

    feat: add populate and simulate methods for Shiva contract transactions

--- packages/overlay-sdk/src/shiva/shiva.ts
+++ packages/overlay-sdk/src/shiva/shiva.ts
@@ -1,6 +1,7 @@
 import {
   Address,
   decodeEventLog,
+  encodeFunctionData,
   getAbiItem,
   getContract,
   GetContractReturnType,
@@ -9,7 +10,7 @@ import {
   WalletClient,
 } from 'viem'
 import { OverlaySDKModule } from '../common/class-primitives/sdk-module'
-import { OverlaySDKCommonProps, TransactionOptions, TransactionResult } from '../core/types'
+import { NoCallback, OverlaySDKCommonProps, TransactionOptions, TransactionResult } from '../core/types'
 import { OverlaySDK } from '../sdk'
 import { ShivaABI } from './abis/Shiva'
 import { CHAINS, ERROR_CODE, invariant, NOOP, SDKError, toWei } from '../common'
@@ -26,6 +27,7 @@ import {
   ShivaBuildSingleOnBehalfOfInnerProps,
   ShivaBuildSingleOnBehalfOfProps,
   ShivaBuildSingleProps,
+  ShivaCancelNonceInnerProps,
   ShivaCancelNonceProps,
   ShivaUnwindOnBehalfOfInnerProps,
   ShivaUnwindOnBehalfOfProps,
@@ -50,14 +52,14 @@ export class OverlaySDKShiva extends OverlaySDKModule {
     this.sdk = sdk
   }
 
-  public async contractAddress() {
-    const chainId = await this.core.rpcProvider.getChainId()
+  public getShivaAddress() {
+    const chainId = this.core.chainId
     invariant(chainId in CHAINS, 'Unsupported chainId')
     return SHIVA_ADDRESS[chainId as CHAINS]
   }
 
-  public async getShivaContract(): Promise<GetContractReturnType<typeof ShivaABI, WalletClient>> {
-    const address = await this.contractAddress()
+  public getShivaContract(): GetContractReturnType<typeof ShivaABI, WalletClient> {
+    const address = this.getShivaAddress()
     return getContract({
       address,
       abi: ShivaABI,
@@ -72,7 +74,7 @@ export class OverlaySDKShiva extends OverlaySDKModule {
     this.core.useWeb3Provider()
     const { account, ...rest } = props
 
-    const address = await this.contractAddress()
+    const address = this.getShivaAddress()
 
     return this.sdk.ovl.approve({
       account: account,
@@ -81,11 +83,33 @@ export class OverlaySDKShiva extends OverlaySDKModule {
     })
   }
 
+  public async populateApproveShiva(props: NoCallback<ShivaApproveProps>) {
+    const { account, amount } = props
+    const address = this.getShivaAddress()
+    
+    return await this.sdk.ovl.populateApprove({
+      account,
+      amount,
+      to: address,
+    })
+  }
+  
+  public async simulateApproveShiva(props: NoCallback<ShivaApproveProps>) {
+    const { account, amount } = props
+    const address = this.getShivaAddress()
+    
+    return await this.sdk.ovl.simulateApprove({
+      account,
+      amount,
+      to: address,
+    })
+  }
+
   public async cancelNonce(props: ShivaCancelNonceProps): Promise<TransactionResult> {
     this.core.useWeb3Provider()
     const { account, ...rest } = props
 
-    const contract = await this.getShivaContract()
+    const contract = this.getShivaContract()
 
     const txArguments = [rest.nonce] as const
 
@@ -98,13 +122,34 @@ export class OverlaySDKShiva extends OverlaySDKModule {
     })
   }
 
+  public async populateCancelNonce(props: NoCallback<ShivaCancelNonceProps>) {
+    return {
+      to: this.getShivaAddress(),
+      from: props.account,
+      data: encodeFunctionData({
+        abi: ShivaABI,
+        functionName: 'cancelNonce',
+        args: [props.nonce],
+      }),
+    }
+  }
+
+  public async simulateCancelNonce(props: NoCallback<ShivaCancelNonceProps>) {
+    const { account, nonce } = await this.parseCancelNonceProps(props)
+    const contract = this.getShivaContract()
+
+    return contract.simulate.cancelNonce([nonce], {
+      account: account.address,
+    })
+  }
+
   // Build
 
   public async build(props: BuildProps): Promise<TransactionResult<BuildResult>> {
     this.core.useWeb3Provider()
     const { callback, account, ...rest } = await this.parseBuildProps(props)
 
-    const contract = await this.getShivaContract()
+    const contract = this.getShivaContract()
 
     const txArguments = [
       {
@@ -128,13 +173,54 @@ export class OverlaySDKShiva extends OverlaySDKModule {
     })
   }
 
+  public async populateBuild(props: NoCallback<BuildProps>) {
+    return {
+      to: this.getShivaAddress(),
+      from: props.account,
+      data: encodeFunctionData({
+        abi: ShivaABI,
+        functionName: 'build',
+        args: [
+          {
+            ovlMarket: props.marketAddress,
+            brokerId: props.brokerId ?? this.core.brokerId,
+            isLong: props.isLong,
+            collateral: props.collateral,
+            leverage: props.leverage,
+            priceLimit: props.priceLimit,
+          },
+        ],
+      }),
+    }
+  }
+
+  public async simulateBuild(props: NoCallback<BuildProps>) {
+    const { account, ...rest } = await this.parseBuildProps(props);
+    const contract = this.getShivaContract()
+
+    const txArguments = [
+      {
+        ovlMarket: rest.marketAddress,
+        brokerId: rest.brokerId ?? this.core.brokerId,
+        isLong: rest.isLong,
+        collateral: rest.collateral,
+        leverage: rest.leverage,
+        priceLimit: rest.priceLimit,
+      },
+    ] as const
+
+    return contract.simulate.build(txArguments, {
+      account: account.address,
+    })
+  }
+
   // Unwind
 
   public async unwind(props: UnwindProps): Promise<TransactionResult> {
     this.core.useWeb3Provider()
     const { account, ...rest } = await this.parseUnwindProps(props)
 
-    const contract = await this.getShivaContract()
+    const contract = this.getShivaContract()
 
     const txArguments = [
       {
@@ -152,6 +238,45 @@ export class OverlaySDKShiva extends OverlaySDKModule {
       getGasLimit: (options: TransactionOptions) =>
         contract.estimateGas.unwind(txArguments, options),
       sendTransaction: (options: TransactionOptions) => contract.write.unwind(txArguments, options),
+    })
+  }
+
+  public async populateUnwind(props: NoCallback<UnwindProps>) {
+    return {
+      to: this.getShivaAddress(),
+      from: props.account,
+      data: encodeFunctionData({
+        abi: ShivaABI,
+        functionName: 'unwind',
+        args: [
+          {
+            ovlMarket: props.marketAddress,
+            brokerId: props.brokerId ?? this.core.brokerId,
+            positionId: props.positionId,
+            fraction: props.fraction,
+            priceLimit: props.priceLimit,
+          },
+        ],
+      }),
+    }
+  }
+
+  public async simulateUnwind(props: NoCallback<UnwindProps>) {
+    const { account, ...rest } = await this.parseUnwindProps(props);
+    const contract = this.getShivaContract()
+    
+    const txArguments = [
+      {
+        ovlMarket: rest.marketAddress,
+        brokerId: rest.brokerId ?? this.core.brokerId,
+        positionId: rest.positionId,
+        fraction: rest.fraction,
+        priceLimit: rest.priceLimit,
+      },
+    ] as const
+
+    return contract.simulate.unwind(txArguments, {
+      account: account.address,
     })
   }
 
@@ -205,7 +330,7 @@ export class OverlaySDKShiva extends OverlaySDKModule {
     }
 
     const transactions = [];
-    const contract = await this.getShivaContract();
+    const contract = this.getShivaContract();
 
     for (let i = 0; i < result.length; i++) {
       const { marketAddress, positionId, priceLimit } = result[i] as UnwindStateSuccess;
@@ -246,7 +371,7 @@ export class OverlaySDKShiva extends OverlaySDKModule {
     this.core.useWeb3Provider()
     const { callback, account, ...rest } = await this.parseBuildSingleProps(props)
 
-    const contract = await this.getShivaContract()
+    const contract = this.getShivaContract()
 
     const txArguments = [
       {
@@ -272,13 +397,56 @@ export class OverlaySDKShiva extends OverlaySDKModule {
     })
   }
 
+  public async populateBuildSingle(props: NoCallback<ShivaBuildSingleProps>) {
+    return {
+      to: this.getShivaAddress(),
+      from: props.account,
+      data: encodeFunctionData({
+        abi: ShivaABI,
+        functionName: 'buildSingle',
+        args: [
+          {
+            ovlMarket: props.params.ovlMarket,
+            brokerId: props.params.brokerId ?? this.core.brokerId,
+            unwindPriceLimit: props.params.unwindPriceLimit,
+            buildPriceLimit: props.params.buildPriceLimit,
+            collateral: props.params.collateral,
+            leverage: props.params.leverage,
+            previousPositionId: props.params.previousPositionId,
+          },
+        ],
+      }),
+    }
+  }
+
+  public async simulateBuildSingle(props: NoCallback<ShivaBuildSingleProps>) {
+    const { account, ...rest } = await this.parseBuildSingleProps(props);
+    const contract = this.getShivaContract()
+
+    const txArguments = [
+      {
+        ovlMarket: rest.params.ovlMarket,
+        brokerId: rest.params.brokerId ?? this.core.brokerId,
+        unwindPriceLimit: rest.params.unwindPriceLimit,
+        buildPriceLimit: rest.params.buildPriceLimit,
+        collateral: rest.params.collateral,
+        leverage: rest.params.leverage,
+        previousPositionId: rest.params.previousPositionId,
+      },
+    ] as const
+
+    return contract.simulate.buildSingle(txArguments, {
+      account: account.address,
+    })
+  }
+
   // Emergency Withdraw
 
   public async emergencyWithdraw(props: EmergencyWithdrawProps): Promise<TransactionResult> {
     this.core.useWeb3Provider()
     const { account, ...rest } = await this.parseEmergencyWithdrawProps(props)
 
-    const contract = await this.getShivaContract()
+    const contract = this.getShivaContract()
 
     const txArguments = [rest.marketAddress, rest.positionId, rest.owner] as const
 
@@ -292,6 +460,29 @@ export class OverlaySDKShiva extends OverlaySDKModule {
     })
   }
 
+  public async populateEmergencyWithdraw(props: NoCallback<EmergencyWithdrawProps>) {
+    return {
+      to: this.getShivaAddress(),
+      from: props.account,
+      data: encodeFunctionData({
+        abi: ShivaABI,
+        functionName: 'emergencyWithdraw',
+        args: [props.marketAddress, props.positionId, props.owner],
+      }),
+    }
+  } 
+
+  public async simulateEmergencyWithdraw(props: NoCallback<EmergencyWithdrawProps>) {
+    const { account, ...rest } = await this.parseEmergencyWithdrawProps(props);
+    const contract = this.getShivaContract()
+
+    const txArguments = [rest.marketAddress, rest.positionId, rest.owner] as const
+
+    return contract.simulate.emergencyWithdraw(txArguments, {
+      account: account.address,
+    })
+  }
+
   // Build On Behalf Of
 
   public async buildOnBehalfOf(
@@ -300,7 +491,7 @@ export class OverlaySDKShiva extends OverlaySDKModule {
     this.core.useWeb3Provider()
     const { callback, account, ...rest } = await this.parseBuildOnBehalfOfProps(props)
 
-    const contract = await this.getShivaContract()
+    const contract = this.getShivaContract()
 
     const txArguments = [
       {
@@ -325,13 +516,56 @@ export class OverlaySDKShiva extends OverlaySDKModule {
     })
   }
 
+  public async populateBuildOnBehalfOf(props: NoCallback<ShivaBuildOnBehalfOfProps>) {
+    return {
+      to: this.getShivaAddress(),
+      from: props.account,
+      data: encodeFunctionData({
+        abi: ShivaABI,
+        functionName: 'build',
+        args: [
+          {
+            ovlMarket: props.params.marketAddress,
+            brokerId: props.params.brokerId ?? this.core.brokerId,
+            isLong: props.params.isLong,
+            collateral: props.params.collateral,
+            leverage: props.params.leverage,
+            priceLimit: props.params.priceLimit,
+          },
+          props.onBehalfOf,
+        ],
+      }),
+    }
+  }
+
+  public async simulateBuildOnBehalfOf(props: NoCallback<ShivaBuildOnBehalfOfProps>) {
+    const { account, ...rest } = await this.parseBuildOnBehalfOfProps(props);
+    const contract = this.getShivaContract()
+
+    const txArguments = [
+      {
+        ovlMarket: rest.params.marketAddress,
+        brokerId: rest.params.brokerId ?? this.core.brokerId,
+        isLong: rest.params.isLong,
+        collateral: rest.params.collateral,
+        leverage: rest.params.leverage,
+        priceLimit: rest.params.priceLimit,
+      },
+      rest.onBehalfOf,
+    ] as const
+
+    return contract.simulate.build(txArguments, {
+      account: account.address,
+    })
+  }
+  
   // Unwind On Behalf Of
 
   public async unwindOnBehalfOf(props: ShivaUnwindOnBehalfOfProps): Promise<TransactionResult> {
     this.core.useWeb3Provider()
-    const { account, ...rest } = await this.parseUnwindOnBehalfOfProps(props)
+    const { account, callback, ...rest } = await this.parseUnwindOnBehalfOfProps(props)
 
-    const contract = await this.getShivaContract()
+    const contract = this.getShivaContract()
 
     const txArguments = [
       {
@@ -347,9 +581,51 @@ export class OverlaySDKShiva extends OverlaySDKModule {
     return this.core.performTransaction({
       ...rest,
       account,
+      callback,
       getGasLimit: (options: TransactionOptions) =>
         contract.estimateGas.unwind(txArguments, options),
       sendTransaction: (options: TransactionOptions) => contract.write.unwind(txArguments, options),
+    })
+  }
+
+  public async populateUnwindOnBehalfOf(props: NoCallback<ShivaUnwindOnBehalfOfProps>) {
+    return {
+      to: this.getShivaAddress(),
+      from: props.account,
+      data: encodeFunctionData({
+        abi: ShivaABI,
+        functionName: 'unwind',
+        args: [
+          {
+            ovlMarket: props.params.marketAddress,
+            brokerId: props.params.brokerId ?? this.core.brokerId,
+            positionId: props.params.positionId,
+            fraction: props.params.fraction,
+            priceLimit: props.params.priceLimit,
+          },
+          props.onBehalfOf,
+        ],
+      }),
+    }
+  }
+
+  public async simulateUnwindOnBehalfOf(props: NoCallback<ShivaUnwindOnBehalfOfProps>) {
+    const { account, ...rest } = await this.parseUnwindOnBehalfOfProps(props);
+    const contract = this.getShivaContract()
+
+    const txArguments = [
+      {
+        ovlMarket: rest.params.marketAddress,
+        brokerId: rest.params.brokerId ?? this.core.brokerId,
+        positionId: rest.params.positionId,
+        fraction: rest.params.fraction,
+        priceLimit: rest.params.priceLimit,
+      },
+      rest.onBehalfOf,
+    ] as const
+
+    return contract.simulate.unwind(txArguments, {
+      account: account.address,
     })
   }
 
@@ -361,7 +637,7 @@ export class OverlaySDKShiva extends OverlaySDKModule {
     this.core.useWeb3Provider()
     const { callback, account, ...rest } = await this.parseBuildSingleOnBehalfOfProps(props)
 
-    const contract = await this.getShivaContract()
+    const contract = this.getShivaContract()
 
     const txArguments = [
       {
@@ -386,6 +662,59 @@ export class OverlaySDKShiva extends OverlaySDKModule {
         contract.write.buildSingle(txArguments, options),
       decodeResult: async (receipt) => this.submitParse(receipt),
     })
+  }
+
+  public async populateBuildSingleOnBehalfOf(props: NoCallback<ShivaBuildSingleOnBehalfOfProps>) {
+    return {
+      to: this.getShivaAddress(),
+      from: props.account,
+      data: encodeFunctionData({
+        abi: ShivaABI,
+        functionName: 'buildSingle',
+        args: [
+          {
+            ovlMarket: props.params.ovlMarket,
+            brokerId: props.params.brokerId ?? this.core.brokerId,
+            unwindPriceLimit: props.params.unwindPriceLimit,
+            buildPriceLimit: props.params.buildPriceLimit,
+            collateral: props.params.collateral,
+            leverage: props.params.leverage,
+            previousPositionId: props.params.previousPositionId,
+          },
+          props.onBehalfOf,
+        ],
+      }),
+    }
+  }
+
+  public async simulateBuildSingleOnBehalfOf(props: NoCallback<ShivaBuildSingleOnBehalfOfProps>) {
+    const { account, ...rest } = await this.parseBuildSingleOnBehalfOfProps(props);
+    const contract = this.getShivaContract()
+
+    const txArguments = [
+      {
+        ovlMarket: rest.params.ovlMarket,
+        brokerId: rest.params.brokerId ?? this.core.brokerId,
+        unwindPriceLimit: rest.params.unwindPriceLimit,
+        buildPriceLimit: rest.params.buildPriceLimit,
+        collateral: rest.params.collateral,
+        leverage: rest.params.leverage,
+        previousPositionId: rest.params.previousPositionId,
+      },
+      rest.onBehalfOf,
+    ] as const
+
+    return contract.simulate.buildSingle(txArguments, {
+      account: account.address,
+    })
+  }
+
+  public async parseCancelNonceProps(props: ShivaCancelNonceProps): Promise<ShivaCancelNonceInnerProps> {
+    return {
+      ...props,
+      account: await this.core.useAccount(props.account),
+      callback: props.callback ?? NOOP,
+    }
   }
 
   private async parseBuildProps(props: BuildProps): Promise<BuildInnerProps> {
@@ -491,8 +820,8 @@ export class OverlaySDKShiva extends OverlaySDKModule {
 
   // Sign methods
   public async getDomain() {
-    const chainId = await this.core.rpcProvider.getChainId()
-    const address = await this.contractAddress()
+    const chainId = this.core.chainId
+    const address = this.getShivaAddress()
 
     return {
       name: 'Shiva',
